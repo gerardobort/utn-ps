693b09ca26d702e29ef22fa0cf3a91549fe1c1fb
Gerardo Bort <gerardobort@gmail.com>
2013-09-20 00:09:08 -0300

    heavy improvements and bugfixes

--- public/ps6/main.js
+++ public/ps6/main.js
@@ -97,8 +97,8 @@ CanvasImage.prototype.transform = function() {
     for (var i = 0, l = this.buffersN-1; i < l; i++) {
         this.buffers[i] = this.buffers[i+1];
     }
-    this.buffers[i-1] = this.context.createImageData(this.original.width, this.original.height);
-    this.buffers[i-1].data.set(this.original.data);
+    this.buffers[this.buffersN-1] = this.context.createImageData(this.original.width, this.original.height);
+    this.buffers[this.buffersN-1].data.set(this.original.data);
 
     this.i++;
 
@@ -108,7 +108,7 @@ CanvasImage.prototype.transform = function() {
         newpx = newdata.data,
         len = newpx.length;
 
-    var epsilon = 60,
+    var epsilon = 40,
         alpha = 0,
         beta = 160,
         gamma = 3,
@@ -127,7 +127,7 @@ CanvasImage.prototype.transform = function() {
                 [5*w/6, h/2] ,
             ], 
         refpointsPoints = [],
-        maxrefpoints = 8;
+        maxrefpoints = 3;
         maxrefpointsPoints = 0;
 
     // initialize refpointsPoints when taking refpoints from previous frames
@@ -137,11 +137,7 @@ CanvasImage.prototype.transform = function() {
 
     // iterate through the main buffer and calculate the differences with previous
     for (i = 0; i < len; i += 4) {
-        newpx[i+0] = 0;
-        newpx[i+1] = 255;
-        newpx[i+2] = 0;
-
-        // change the alpha channel based on the frame differences
+        // change the alpha channel based on the frame color differences
         alpha = 255;
         for (var j = 0, l = this.buffersN-1; j < l; j++) {
             if (distance3(this.buffers[j].data, this.buffers[j+1].data, i) < epsilon) {
@@ -154,18 +150,16 @@ CanvasImage.prototype.transform = function() {
         x = (i/4) % w;
         y = parseInt((i/4) / w);
         if ((!(x % omega) && !(y % omega)) && alpha > beta) {
-            newpx[i+0] = 255;
-            newpx[i+1] = 0;
-            newpx[i+2] = 0;
-            newpx[i+3] = 255;
+            newpx[i+0] = oldpx[i+0];
+            newpx[i+1] = oldpx[i+1];
+            newpx[i+2] = oldpx[i+2];
+            newpx[i+3] = oldpx[i+3];
 
             var added = false;
             for (var j = 0, l = refpoints.length; j < l; j++) {
-                if (distance2([x, y], refpoints[j], 0) < 100) { // greek const
+                if (distance2([x, y], refpoints[j], 0) < 90) { // greek const
                     grid[i/4] = j;
                     refpointsPoints[j].push({ x: x, y: y });
-                    //refpoints[j][0] = (refpoints[j][0] + x)/2;
-                    //refpoints[j][1] = (refpoints[j][1] + y)/2;
                     added = true;
                     if (refpointsPoints.length > maxrefpointsPoints) {
                         maxrefpointsPoints = refpointsPoints.length;
@@ -186,7 +180,7 @@ CanvasImage.prototype.transform = function() {
     var ctx = this.context;
 
     for (var j = 0, l = refpoints.length; j < l; j++) {
-        //markPoint(ctx, refpoints[j][0], refpoints[j][1], 5, 'red');
+        markPoint(ctx, refpoints[j][0], refpoints[j][1], 3, 'red');
     }
 
     // store the count number of matched points
@@ -200,16 +194,13 @@ CanvasImage.prototype.transform = function() {
 
 
     // remove groups with not enough elements
-    /*
-    for (var i = 0, l = refpointsPoints.length, r = 0; i < l - r ; i++) {
-        if (refpoints.length < 10) {
+    for (var i = 0, l = refpointsPoints.length; i < l; i++) {
+        if (refpointsPoints[i].length < 8) {
             refpoints.splice(i,1);
             refpointsPoints.splice(i,1);
-            r++;
-            //i--;
+            i--; l--;
         }
     }
-    */
 
     // based on the sumatory of points, calculate the convex hull and paint it
     for (var i = 0, l = refpointsPoints.length; i < l; i++) {
@@ -220,28 +211,51 @@ CanvasImage.prototype.transform = function() {
         if (indices && indices.length > 0 && indices.length > 3) {
             ctx.beginPath();
             ctx.moveTo(rpoints[indices[0]].x, rpoints[indices[0]].y);
-            var p = avgp = center = [w/2, h/2];
+            var p, j, b1, b2, avgp = [w/2, h/2], center = [w/2, h/2], avgc = [0, 0, 0];
+
+            b1 = this.buffers[this.buffersN-1].data;
+            b2 = this.buffers[this.buffersN-2].data;
 
             // calculate avgp
             for (var i2 = 1, l2 = indices.length; i2 < l2; i2++) {
                 p = [rpoints[indices[i2]].x, rpoints[indices[i2]].y];
                 avgp[0] += (p[0] - center[0])/l;
                 avgp[1] += (p[1] - center[1])/l;
+
+                j = (y*w+x)*4;
+
+                avgc[0] += b1[j+0]/l2;
+                avgc[1] += b1[j+1]/l2;
+                avgc[2] += b1[j+2]/l2;
+
+                if (
+                    distance3(b1, b2, j) < 80
+                    && distance3(b1, b2, j) < 20
+                ) {
+                    ctx.lineTo(p[0], p[1]);
+                }
             }
 
+            /*
             // discard bad points
             for (var i2 = 1, l2 = indices.length; i2 < l2; i2++) {
                 p = [rpoints[indices[i2]].x, rpoints[indices[i2]].y];
-                if (distance2(p, avgp, 0) > 20) continue;
-            //    ctx.lineTo(p[0], p[1]);
+                // if (distance2(p, avgp, 0) < 200) {;
+                if (distance3(newpx[i2], oldpx[i2], 0) < 100) {;
+                    ctx.lineTo(p[0], p[1]);
+                }
             }
+            */
             ctx.closePath();
-            var color = "rgba(0, 100, 0, 0.9)";
+            //var color = "rgba(0, 100, 0, 0.9)";
+            var color = 'rgba(' + parseInt(avgc[0]) + ',' + parseInt(avgc[1]) + ',' + parseInt(avgc[2]) + ', 0.3)';
+
+/*
             if (1 === i) { color = "rgba(100, 0, 0, 0.9)"; }
             if (2 === i) { color = "rgba(0, 0, 100, 0.9)"; }
             if (3 === i) { color = "rgba(100, 100, 0, 0.9)"; }
             if (4 === i) { color = "rgba(0, 100, 100, 0.9)"; }
-
+*/
             ctx.fillStyle = color;
             
 
